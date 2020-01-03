697b9b606d78e2b6e33820977b4886f62a836f39
animevn <haanhth21@gmail.com>
2020-01-03 14:51:07 +0700

    add viewmodel

---
+++ app/src/main/java/com/haanhgs/recyclerroomdemo/viewmodel/ViewModel.java
@@ -0,0 +1,46 @@
+package com.haanhgs.recyclerroomdemo.viewmodel;
+
+import android.app.Application;
+
+import com.haanhgs.recyclerroomdemo.model.Word;
+import com.haanhgs.recyclerroomdemo.model.WordRepo;
+import com.haanhgs.recyclerroomdemo.model.WordRoom;
+
+import java.util.List;
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+public class ViewModel extends AndroidViewModel {
+
+    private final WordRepo repo;
+    private final LiveData<List<Word>> allData;
+
+    public ViewModel(@NonNull Application application) {
+        super(application);
+        repo = new WordRepo(application);
+        allData = repo.getAllData();
+    }
+
+    public LiveData<List<Word>> getAllData() {
+        return allData;
+    }
+
+    public void delete(Word word){
+        repo.delete(word);
+    }
+
+    public void deleteAll(){
+        repo.deleteAll();
+    }
+
+    public void insert(Word word){
+        repo.insert(word);
+    }
+
+    public void insert(String string){
+        repo.insert(string);
+    }
+
+
+}
